a2a90e2ac39e0f378766c8298d60993d454e9410
e.pol <eugene.poletski@gmail.com>
2015-08-18 16:29:41 +0300

    Add filter controller script

--- js/catalog.js
+++ js/catalog.js
@@ -1,55 +1,52 @@
-var projects = [
-	{
-		id: "hp-15-001",
-		area: 100
-	},
-	{
-		id: "hp-15-002",
-		area: 150
-	},
-	{
-		id: "hp-15-003",
-		area: 100
+var main = function() {
+	"use strict";
+
+	// init
+
+	var filterCatalog = function(filterId, filterApply) {
+		console.log("filterId:" + filterId + " filterApply:" + filterApply);
 	}
-];
 
-function Catalog(catalog) {
-	this._catalog = catalog;
-}
+	var filterCatalogInterval = function(filterId, interval) {
+		console.log("filterId:" + filterId + " min:" + interval[0] + " max:" + interval[1]);
+	}
 
-Catalog.prototype.getItemById = function(itemId) {
-	var catalog = this._catalog;
-	for (var i = 0 ; i < catalog.length ; i++) {
-		if (catalog[i].id === itemId) {
-			return catalog[i];
-		};
-	};
+	// filter
+
+	var catalogFilter = document.getElementById("filters");
+
+	catalogFilter.addEventListener("change", function (event) {
+		
+		var firedEl = event.target;
+		var firedElId = firedEl.id;
+		var firedElParent = firedEl.parentElement;
+		var firedElParentId = firedElParent.id;
+		
+		switch(firedElParentId) {
+			
+			case "area":
+			var childrenElems = firedElParent.children;
+			var interval = [];
+
+			for (var i = 0; i < childrenElems.length; i++) {
+				if (childrenElems[i].tagName === "INPUT" && childrenElems[i].type === "number") {
+					interval.push(childrenElems[i].value);
+				};
+
+			};
+
+			filterCatalogInterval(firedElParentId, interval);
+			break;
+
+			default:
+			filterCatalog(firedElId, firedEl.checked);
+			break;
+
+		}
+
+
+	}, true);
+
 };
 
-Catalog.prototype.getItemsByPropertyValue = function(property, value) {
-	var catalog = this._catalog;
-	var arr = [];
-
-	catalog.forEach(function(item) {
-		if (item[property] === value) {
-			arr.push(item);
-		};
-	});
-
-	return arr;
-};
-
-Catalog.prototype.createSelection = function(selectionName) {
-	
-}
-
-var homeplans = new Catalog(projects);
-var item1 = homeplans.getItemById('hp-15-001');
-
-console.log(item1.area);
-
-var itemsList1 = homeplans.getItemsByPropertyValue('area', 100);
-
-console.log(itemsList1[0].id);
-console.log(itemsList1[1].id);
-
+main();
