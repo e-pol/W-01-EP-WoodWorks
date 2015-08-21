e9338ff04ccd45523a49fb00779afd6ba25bfe16
e.pol <eugene.poletski@gmail.com>
2015-08-21 10:41:54 +0300

    Remove part of old code of catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -29,7 +29,7 @@ var main = function() {
 
 	// catalog
 
-	function Catalog(id, items) {
+	/*function Catalog(id, items) {
 		this._id = id;
 		this._node = document.getElementById(id);
 		this._items = items;
@@ -53,45 +53,12 @@ var main = function() {
 		console.log("Object Catalog id=\"" + this._id + "\" method: .appendCatalogHtml");
 		// remove old
 		// append new
-	}
-
-
-	// filters
-
-	function Filter(id, initialCatalog) {
-		this._id = id;
-		this._node = document.getElementById(id);
-		this._filters = [];
-		this._eventListeners = "";
-		this._catalogs = [];
-		this._catalogs[0] = initialCatalog;
-
-		console.log("Created: Object Filter id=\"" + this._id + "\"");
-	}
-
-	Filter.prototype.applyChanges = function() {
-		console.log("Object Filter id=\"" + this._id + "\" method: .applyChanges");
-
-		this._catalogs[0].applyFilters(this._filters);
-	}
-
-	Filter.prototype.setFilterPropertyValue = function(property, value) {
-		console.log("Object Filter id=\"" + this._id + "\" method: .setFilterPropertyValue");
-		console.log("Received: property=" + property + " value=" + value);	
-		this.applyChanges();	
-	}
-
-
-	Filter.prototype.setFilterPropertyInterval = function(property, min, max) {
-		console.log("Object Filter id=\"" + this._id + "\" method: .setFilterPropertyInterval");
-		console.log("Received: property=" + property + " min=" + min + " max=" + max);
-		this.applyChanges();
-	}
+	}*/
 
 	// init
 
-	var hpCatalog = new Catalog("catalog", houseplans);
-	var hpFilter = new Filter("filters", hpCatalog);
+	/*var hpCatalog = new Catalog("catalog", houseplans);
+	var hpFilter = new Filter("filters", hpCatalog);*/
 
 
 	// filter
@@ -118,11 +85,11 @@ var main = function() {
 
 			};
 
-			hpFilter.setFilterPropertyInterval(firedElParentId, interval[0], interval[1]);
+			// hpFilter.setFilterPropertyInterval(firedElParentId, interval[0], interval[1]);
 			break;
 
 			default:
-			hpFilter.setFilterPropertyValue(firedElParentId, firedEl.value);
+			// hpFilter.setFilterPropertyValue(firedElParentId, firedEl.value);
 			break;
 
 		}
